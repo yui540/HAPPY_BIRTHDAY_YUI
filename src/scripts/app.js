c1d2ef797bf36b1c0a9638b331bf0c7d6d2778c9
yuki540 <tabletennis540@gmail.com>
2018-06-10 09:39:38 +0900

    update: 次のパートへの切り替え処理を追加

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,3 +5,8 @@ import preload_data from './config/preload'
 util.preload(preload_data, () => {
   console.log('fin.')
 }, data => {})
+
+const load_view__end = document.querySelector('.load-view__end div:nth-child(6)')
+load_view__end.addEventListener('animationend', () => {
+  util.finLoadView()
+})
